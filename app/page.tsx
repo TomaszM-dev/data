6cc9e189f1740d2ffe2bf373f108f89c5368a774
TomaszM-dev <tomekmalocha@gmail.com>
2023-10-16 16:28:37 +0200

    learning for coding interv

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
-export default function Home() {
-  function myFunction(a: any) {
-    const res = a.
-  }
-  myFunction(['I', 'need', 'candy'])
+import { COOKIE_NAME_PRERENDER_BYPASS } from "next/dist/server/api-utils";
+import { Jacques_Francois } from "next/font/google";
+
+interface myFunctionProps {
+  a: [];
+  b: [];
+}
+
+export default function Home(a, b) {
+  const myFunction = (a, b) => {
+    console.log(a);
+  };
+
+  myFunction(new Date("2000/01/01 08:00:00"), new Date("2000/01/01 08:45:00"));
 
   return <div>hello</div>;
 }
